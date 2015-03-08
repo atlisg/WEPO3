c2659c2790ba400c9e5d86250556d412a4f08f46
bergthor13 <bergthor13@ru.is>
2015-03-08 20:43:23 +0000

    evaluationResource factory added

--- src/js/app.js
+++ src/js/app.js
@@ -10,31 +10,43 @@ angular.module('evaluationApp').config(['$routeProvider',
 	}
 ]);
 
+angular.module('evaluationApp').factory('evaluationResource',
+	function($http) {
+		var factory = {};
+
+		factory.loginUser = function(loginObject) {
+			return $http.post('http://dispatch.ru.is/demo/api/v1/login', loginObject);
+		};
+
+		factory.getEvaluations = function(token) {
+			var tok = 'Basic ' + token;
+			var config = {headers         : {'Authorization': tok},
+			              withCredentials : true
+	        };
+			return $http.get('http://dispatch.ru.is/demo/api/v1/my/evaluations', config);
+		};
+
+		return factory;
+	}
+);
+
 angular.module('evaluationApp').controller('loginController', [
-	'$scope', '$location', '$rootScope', '$routeParams', '$http',
-	function ($scope, $location, $rootScope, $routeParams, $http) {
+	'$scope', '$location', '$rootScope', '$routeParams', '$http', 'evaluationResource',
+	function ($scope, $location, $rootScope, $routeParams, $http, evaluationResource) {
 	$scope.token = '';
+
 	$rootScope.login = function() {
 		var loginObject = {user:'bergthor13', pass:'123456'};
-		$http.post('http://dispatch.ru.is/demo/api/v1/login', loginObject)
-		.then(function(response) {
+
+		evaluationResource.loginUser(loginObject).then(function(response) {
 			console.log(response);
 			$scope.token = response.data.Token;
 			console.log('Basic ' + $scope.token);
 		});
-		var tok = 'Basic ' + $scope.token;
-		// var config = {method:         'GET',
-		//               url:            'http://dispatch.ru.is/demo/api/v1/my/evaluations',
-		//               headers:        {'Authorization': tok},
-		//               withCredentials:false};
 
-		var config = {headers:{'Authorization': tok,
-		                       'Accept': 'application/json;odata=verbose'}, withCredentials:true};
-
-		$http.get('http://dispatch.ru.is/demo/api/v1/my/evaluations', config)
-		 .then(function(response) {
-		 	console.log(response);
-		 });
+		evaluationResource.getEvaluations($scope.token).then(function(response) {
+			console.log(response);
+		});
 	};
 
 }]);
