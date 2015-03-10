9e28d0b1ffc69b9343da2eb3ae064611fb3a6d64
agirmar92 <aegir13@ru.is>
2015-03-10 13:37:54 +0000

    taka til

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ angular.module('evaluationApp', ['ngRoute']);
 
 angular.module('evaluationApp').config(['$routeProvider',
 	function ($routeProvider) {
+
 		$routeProvider
 			.when('/login',             { templateUrl: '../html/login.html',   controller: 'loginController' })
 			.when('/evaluations/:user', { templateUrl: 'evaluations.html', controller: 'loginController' })
@@ -12,14 +13,17 @@ angular.module('evaluationApp').config(['$routeProvider',
 ]);
 
 angular.module('evaluationApp').factory('evaluationResource',
-	function($http) {
+	function ($http) {
+
 		var factory = {};
 
 		factory.loginUser = function(loginObject) {
 			return $http.post('http://dispatch.ru.is/demo/api/v1/login', loginObject);
-		};
+		}
 
-		factory.getEvaluations = function() {
+		factory.getEvaluations = function(token) {
+			console.log("Inni fallinu: " + token);
+			$http.defaults.headers.common.Authorization = "Basic " + token;
 			return $http.get('http://dispatch.ru.is/demo/api/v1/my/evaluations');
 		};
 
@@ -30,27 +34,19 @@ angular.module('evaluationApp').factory('evaluationResource',
 angular.module('evaluationApp').controller('loginController', [
 	'$scope', '$location', '$rootScope', '$routeParams', '$http', 'evaluationResource',
 	function ($scope, $location, $rootScope, $routeParams, $http, evaluationResource) {
-	$scope.token = '';
 
 	$scope.login = function() {
 		var loginObject = {user:'bergthor13', pass:'123456'};
 
-		evaluationResource.loginUser(loginObject).then(function(response) {
-			console.log(response);
-			$scope.token = response.data.Token;
-			var theToken = 'Basic ' + $scope.token;
-			console.log(theToken);
-			// adding the token to the header.
-			$http.defaults.headers.common.Authorization = theToken;
-
-			evaluationResource.getEvaluations().success(function(data) {
+		evaluationResource.loginUser(loginObject).success(function(data) {
+			evaluationResource.getEvaluations(data.Token).success(function(data) {
 				// Should be Doddi2.
 				console.log(data[0].TemplateName);
+				// Bara leika mer
+				$location.path('/evaluations/' + loginObject.user);
 			});
-
-			// Bara leika mer
-			$location.path('/evaluations/' + loginObject.user);
 		});
+
 
 	};
 
