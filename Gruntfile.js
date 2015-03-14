ced4dee9a27c6938fb1a3592de4b13137b7f932b
bergthor13 <bergthor13@ru.is>
2015-03-14 18:40:30 +0000

    concat, uglify, restart og meira skemmtilegt

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	var taskConfig = {
+		jshint: {
+			src: ['src/js/*', '!src/js/app.concat.js', '!src/js/app.min.js'],
+			gruntfile: ['Gruntfile.js'],
+			options: {
+				curly:  true,
+				immed:  true,
+				newcap: true,
+				noarg:  true,
+				sub:    true,
+				boss:   true,
+				eqnull: true,
+				node:   true,
+				undef:  true,
+				globals: {
+					_:       false,
+					jQuery:  false,
+					angular: false,
+					moment:  false,
+					console: false,
+					$:       false,
+					io:      false,
+					document:false,
+				}
+			}
+		},
+		concat: {
+			bar: {
+				src: ['src/js/*.js', '!src/js/app.concat.js', '!src/js/app.min.js'],
+				dest: 'src/js/app.concat.js',
+			},
+		},
+		uglify: {
+			bar: {
+				src: ['src/js/app.concat.js'],
+				dest: 'src/js/app.min.js',
+			}
+		},
+		cssmin: {
+			options: {
+				shorthandCompacting: false,
+				roundingPrecision: -1
+			},
+			target: {
+				files: {
+					'src/css/style.min.css': ['src/css/style.css']
+				}
+			}
+		}
+	};
+	grunt.initConfig(taskConfig);
+};
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # WEPO3
 Assignment 3 in Web Programming II - Teacher evaluation app
+
+## How to run
+To run the website make sure you have npm installed.
+
+See [NPM GitHub Repo](https://github.com/npm/npm) for details.
+
+Execute the following command in the root of the project.
+```
+./init
+```
+
+To run the project execute the following command.
+```
+./startClient
+```
+You should now be able to open the website at the following URL
+
+[http://localhost:8000/src/html](http://localhost:8000/src/html)
