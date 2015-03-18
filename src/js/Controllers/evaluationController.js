a4cc73d43f2166d5b0ee4f842173cd2d512c2346
Atli Sævar Guðmundsson <atliepli@Atli-Svar-Gumundssons-MacBook-Pro.local>
2015-03-18 23:35:48 +0000

    smá lag

--- src/js/Controllers/evaluationController.js
+++ src/js/Controllers/evaluationController.js
@@ -73,44 +73,44 @@ angular.module('evaluationApp').controller('evaluationController', [
 				console.log("ANS");
 				console.log($scope.answers);
 			});
-});
+		});
 
-$scope.postEvaluation = function() {
+		$scope.postEvaluation = function() {
 
-	var arr = [];
+			var arr = [];
 
-	for (var key in $scope.answers) {
-		if($scope.answers.hasOwnProperty(key)) {
-			// console.log($scope.answers[key]);
-			arr.push($scope.answers[key]);
-		}
-	}
-
-	var concatted = $scope.evaluationFromServer.CourseQuestions.concat($scope.evaluationFromServer.TeacherQuestions);
-	var returnObject = arr;
-
-	for (var i = 0; i < arr.length; i++) {
-		if (arr[i].Value === null) {continue;}
-		if (arr[i].Value.constructor === Array) {
-			for (var j = 0; j < concatted.length; j++) {
-				for (var k = 0; k < concatted[j].Answers.length; k++) {
-					if (arr[i].Value[k] === true) {
-						j=0;
-						while(arr[i].QuestionID !== concatted[j].ID) {j=j+1;}
-						arr[i].Value[k] = concatted[j].Answers[k].ID;
-					}
+			for (var key in $scope.answers) {
+				if($scope.answers.hasOwnProperty(key)) {
+					// console.log($scope.answers[key]);
+					arr.push($scope.answers[key]);
 				}
 			}
-						console.log(arr[i].Value);
-			returnObject[i].Value = arr[i].Value.join();
-			console.log("CONCATTED");
-			console.log(concatted);
-		}
-	}
-	studentResource.postEvaluation($scope.courseID, $scope.semesterID, $scope.evalID, returnObject).success(function(data) {
-		$location.path("/evaluations");
-	});
 
-};
-}
+			var concatted = $scope.evaluationFromServer.CourseQuestions.concat($scope.evaluationFromServer.TeacherQuestions);
+			var returnObject = arr;
+
+			for (var i = 0; i < arr.length; i++) {
+				if (arr[i].Value === null) {continue;}
+				if (arr[i].Value.constructor === Array) {
+					for (var j = 0; j < concatted.length; j++) {
+						for (var k = 0; k < concatted[j].Answers.length; k++) {
+							if (arr[i].Value[k] === true) {
+								j=0;
+								while(arr[i].QuestionID !== concatted[j].ID) {j=j+1;}
+								arr[i].Value[k] = concatted[j].Answers[k].ID;
+							}
+						}
+					}
+								console.log(arr[i].Value);
+					returnObject[i].Value = arr[i].Value.join();
+					console.log("CONCATTED");
+					console.log(concatted);
+				}
+			}
+			studentResource.postEvaluation($scope.courseID, $scope.semesterID, $scope.evalID, returnObject).success(function(data) {
+				$location.path("/evaluations");
+			});
+
+		};
+	}
 ]);
