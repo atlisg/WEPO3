f4a4006cbc066988d9056b073282d96781037cc6
agirmar92 <aegir13@ru.is>
2015-03-18 14:49:00 +0000

    CSS compilter maettur

--- startClient.bat
+++ startClient.bat
@@ -1,4 +1,6 @@
+lessc src/css/style.less > src/css/style.css
 grunt jshint &&
 grunt concat &&
 grunt uglify &&
+grunt cssmin &&
 python -m http.server 8000
--- test/app.spec.js
+++ test/app.spec.js
@@ -1,112 +1,70 @@
-describe('authenticationController', function() {
-  var $controller, $scope, $location;
+/*describe("controller: authenticationController", function() {
+  // Write tests
+  var $scope, myService, $location
 
-  beforeEach(module('evaluationApp', ['ngRoute', 'chart.js']));
-  beforeEach(inject(function(_$controller_, _$scope_, _$location_){
-  	// Create a new scope:
-  	$scope = _$scope_;
-    $controller = _$controller_;
+  beforeEach(module('evaluationApp'));
+
+  beforeEach(inject(function($rootScope, _myService_, _$location_) {
+    $scope = $rootScope.$new();
+    myService = _myService_;
     $location = _$location_;
-
-  	//ctrl = $controller("authenticationController", { $scope: $scope });
   }));
-  // The ctrl variable should now be accessible to
-  // all test specs in this suite, and should reference
-  // our authenticationController
 
-  describe('virkadu plis', function() {
-    var $scope, controller;
+  it('should work', function() {
+    // Do something
+    // Expect something
+  });
+})*/
 
-    beforeEach(function() {
-      $scope = {
-        user: '',
-        pass: '',
-        errorMessage: '',
-        warningMessage: '',
-        submitted: false
-      };
+describe('Posts', function() {
+  var rest, post;
 
-      controller = $controller('authenticationController', {
-        $scope: $scope,
-        $rootScope: $rootScope,
-        $location: $location
-      });
-    });
+  beforeEach(function() {
+    rest = new RestService();
+    post = new Post(rest);
+  });
 
-    it("Should ..", function(){
-    	var result = $scope.testShit();
-    	expect(result).toBe(1992);
-    	expect(result).not.toBe(1993);
-    });
+  it('will initialize the rest service upon creation', function() {
+    spyOn(rest, 'init');
+    post = new Post(rest);
+    expect(rest.init).toHaveBeenCalled();
+  });
+
+  it('will receive the list of posts from the rest service', function() {
+    var posts = [
+      {
+        title: 'Foo',
+        body: 'Foo post'
+      },
+      {
+        title: 'Bar',
+        body: 'Bar post'
+      }
+    ];
+
+    spyOn(rest, 'getAll').and.returnValue(posts);
+    post.retrieve();
+    expect(rest.getAll).toHaveBeenCalled();
+    expect(post.posts).toBe(posts);
+  });
+
+  it('can accept a post to update it', function() {
+    var postToAccept = {
+      title: 'Title',
+      body: 'Post'
+    };
+    spyOn(rest, 'update');
+    post.accept(postToAccept);
+    expect(rest.update).toHaveBeenCalledWith(postToAccept);
+  });
+
+  it('can receive a callback upon accept', function() {
+    var fn = jasmine.createSpy();
+    var postToAccept = {
+      title: 'Title',
+      body: 'Post'
+    };
+    post.accept(postToAccept, fn);
+    expect(fn).toHaveBeenCalled();
   });
 });
-
-/*describe('evalutionResource tests', function (SERVER_URL){
-  var evaluationResource,
-      httpBackend;
-  
-  // excuted before each "it" is run.
-  beforeEach(function (){
-    
-    // load the module.
-    module('evalutionApp');
-    
-    inject(function($httpBackend, _evaluationResource_) {
-      evalutionResource = _evaluationResource_;      
-      httpBackend = $httpBackend;
-    });
-  });
-
-  // make sure no expectations were missed in your tests.
-  // (e.g. expectGET or expectPOST)
-  afterEach(function() {
-	httpBackend.verifyNoOutstandingExpectation();
-	httpBackend.verifyNoOutstandingRequest();
-  });
-     
-  // check to see if it has the expected function
-  it('should have an loginUser function', function () { 
-    expect(angular.isFunction(evalutionResource.loginUser)).toBe(true);
-  });
-  it('should have an getEvaluations function', function () { 
-    expect(angular.isFunction(evalutionResource.getEvaluations)).toBe(true);
-  });
-  
-  // check to see if it does what it's supposed to do.
-  it('should send username and pw and return user data', function (){
-    // set up some data for the http call to return and test later.
-	var returnData = { Token: 'YWVnaXIxMzoxMjM0NTY=',
-					   User: { Username: 'aegir13',
-					           FullName: 'Ægir Már Jónsson',
-					           SSN: '0507922699',
-					           Email: null,
-					           Role: 'student',
-					           ImageURL: 'http://www.ru.is/kennarar/dabs/img/05/0507922699.jpg'
-					    } 
-					};
-
-    var loginObject = { user: 'aegir13',
-		                pass: '123456' };
-    
-    // expectGET to make sure this is called once.
-    httpBackend.expectPOST(SERVER_URL + 'login', loginObject).respond(returnData);
-    
-    // make the call.
-    var returnedPromise = evalutionResource.loginUser(loginObject);
-    
-    // set up a handler for the response, that will put the result
-    // into a variable in this scope for you to test.
-    var result;
-    returnedPromise.then(function(response) {
-      result = response;
-    });
-    
-    // flush the backend to "execute" the request to do the expectedGET assertion.
-    httpBackend.flush();
-    
-    // check the result. 
-    // (after Angular 1.2.5: be sure to use `toEqual` and not `toBe`
-    // as the object will be a copy and not the same instance.)
-    expect(result).toEqual(returnData);
-  });
-});*/
