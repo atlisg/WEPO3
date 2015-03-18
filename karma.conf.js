146a60eee7289e3692d638ded32fd1867a1e5b44
Atli Sævar Guðmundsson <atliepli@Atli-Svar-Gumundssons-MacBook-Pro.local>
2015-03-18 14:54:48 +0000

    reyna við test errorinn

--- karma.conf.js
+++ karma.conf.js
@@ -16,6 +16,8 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'bower_components/angular/angular.min.js',
+      'bower_components/angular-route/angular-route.min.js',
+      'bower_components/angular-chart.js/angular-chart.js',
       'src/js/app.js',
       'src/**/*.js',
       'test/*.spec.js'
